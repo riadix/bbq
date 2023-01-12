374f979b0cccc1495025bed873342cfd206fc67a
Ria I <ddmlbix@gmail.com>
2023-01-12 21:40:07 +0300

    Users initial

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  resources :users
   # devise_for :users
   root "events#index"
 
   resources :events
-  # resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:show, :edit, :update]
 end
