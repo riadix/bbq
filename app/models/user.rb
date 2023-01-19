405299e0a2699913d4809bb1d57c85aca31c3683
Ria I <ddmlbix@gmail.com>
2023-01-19 17:25:26 +0300

    Layout fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,4 @@ class User < ApplicationRecord
   validates :name,
             presence: true,
             length: { maximum: 35 }
-
-  before_validation :set_name, on: :create
-
-  private
-
-  def set_name
-    self.name = "Friend #{rand(777)}" if self.name.blank?
-  end
 end
